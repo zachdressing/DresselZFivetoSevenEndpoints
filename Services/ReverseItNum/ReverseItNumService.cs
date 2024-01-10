6608a25e83e4db916fdcf23c429d90e2a8011a2c
zachdressing <zed_dressel@hotmail.com>
2024-01-10 10:49:23 -0800

    formattin

--- Services/ReverseItNum/ReverseItNumService.cs
+++ Services/ReverseItNum/ReverseItNumService.cs
@@ -7,6 +7,6 @@ public class ReverseItNumService : IReverseItNumService
         string num2 = num1.ToString();
         Char[] string1 = num2.ToCharArray();
         Array.Reverse(string1);
-        return $"You entered {num1}, reversed is {new string (string1)}.";
+        return $"You entered {num1}, reversed is {new string(string1)}.";
     }
 }
